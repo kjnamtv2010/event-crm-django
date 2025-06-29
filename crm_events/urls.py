11a3e9745b0464f424e66cdac2dde7507d4cee10
Nam <kjnamtv2010@gmail.com>
2025-06-29 09:41:19 +0700

    refactor code

--- crm_events/urls.py
+++ crm_events/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('send-emails/', views.SendEmailsView.as_view(), name='send-emails'),
     path('events/register/<slug:slug>/', views.EventDetailAndRegisterView.as_view()),
     path('events/<slug:slug>/', views.EventDetailPageView.as_view(), name='event-detail-page'),
-    path('api/utm-analysis/', views.UTMAnalysisView.as_view(), name='utm-analysis-api'),
+    path('utm-analysis/', views.UTMAnalysisView.as_view(), name='utm-analysis-api'),
     path('email-tracker/', views.EmailTrackerTemplateView.as_view(), name='email-status-tracker'),
-    path('api/email-logs/', views.email_log_json_view, name='email-log-api'),
+    path('email-logs/', views.email_log_json_view, name='email-log-api'),
 ]
